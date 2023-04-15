ff26b030c770ecfabb2ec220df632d1974a6fe19
annusingh58 <127593919+annusingh58@users.noreply.github.com>
2023-04-15 12:11:19 -0400

    updates

---
+++ server.js
@@ -0,0 +1,25 @@
+// function register(event){
+//     event.preventDefault();
+//     // console.log(document.getElementById("useremail"))
+// var name = console.log(document.getElementById("username"))
+// console.log(name,"name here");
+// var name = console.log(document.getElementById("password"))
+// console.log(name,"password");
+// var name = console.log(document.getElementById("confim password"))
+// console.log(name,"password");
+// }
+
+
+
+var anu=[12,34,57,89,,"JHG"]
+// console.log(anu[0]) //array start[]
+
+
+
+
+// var kunal= {key:"value"}         //object strt{}            
+var institutedata= {institute:"awdiz", noofstudent:100,
+noofclass:2,nameofteacher:["swraj","siddhartg",987,98]
+}
+var array =institutedata,
+
